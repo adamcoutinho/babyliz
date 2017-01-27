b892a5795c30b9fba08bc06d780ef9cfcb2a2044
adam <adamcoutinho.git@gmail.com>
2017-01-27 19:34:31 -0300

    adicao do backandService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
-
+import {BackandService} from '../providers/backand-service'
 import { HomePage } from '../pages/home/home';
 
 
@@ -11,12 +11,16 @@ import { HomePage } from '../pages/home/home';
 export class MyApp {
   rootPage = HomePage;
 
-  constructor(platform: Platform) {
+  constructor(platform: Platform , public backandService:BackandService) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
+      this.backandService.setIsMobile(platform.is('mobile'));						
+      this.backandService.setAppName('babyliz2017');						
+      this.backandService.setAnonymousToken('f5b32139-8aee-41bf-a107-47da8e991e9f');
+      this.backandService.useAnonymousAuth();
     });
   }
 }
